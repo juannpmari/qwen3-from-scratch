182115738433f12d67d334476eed55c79f8ad7ac
juannpmari <juannpmari@gmail.com>
2025-09-17 18:02:59 -0300

    implement GQA head

--- attention/grouped_query_attention.py
+++ attention/grouped_query_attention.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 from rmsnorm import RMSNorm
-from rope import Rope
+from rope import compute_rope
 
 class GQA(nn.Module):
     def __init__(self, emb_dim, gka_ratio = 2, num_heads = 16):
@@ -14,7 +14,7 @@ class GQA(nn.Module):
         self.linear_output_layer = nn.Linear(emb_dim, emb_dim, bias=False)
 
     def forward(self, x):
-        batch_size, seq_len, _ = x.shape
+        batch_size, seq_len, hidden_dim = x.shape # batch_size x seq_len x 1024
 
         #compute Q, K, V marices
         queries = self.W_Q(x) # my_linear.forward(x) -> tensor: seq_len x 1024
@@ -30,27 +30,40 @@ class GQA(nn.Module):
         queries = RMSNorm(self.head_dim)(queries) # batch_size x seq_len x 64 x 16
         keys = RMSNorm(self.head_dim)(keys) # batch_size x seq_len x 64 x 8
 
-        #Compute RoPE embeddings (CHECK THIS)
-        queries = Rope(queries) # batch_size x seq_len x 64 x 16
-        keys = Rope(keys) # batch_size x seq_len x 64 x 8
+        #Compute RoPE embeddings
+        queries = compute_rope(queries) # batch_size x seq_len x 64 x 16
+        keys = compute_rope(keys) # batch_size x seq_len x 64 x 8
 
         
         #reshape for attention computation
-        queries.view(batch_size, seq_len, self.head_dim, self.num_heads//self.gka_ratio, self.gka_ratio) # batch_size x seq_len x 64 x 8 x 2
-        keys.view(batch_size, seq_len, self.head_dim, self.num_heads//self.gka_ratio,1) # batch_size x seq_len x 64 x 8 x 1
+        queries = queries.view(batch_size, seq_len, self.head_dim, self.num_heads//self.gka_ratio, self.gka_ratio) # batch_size x seq_len x 64 x 8 x 2
+        keys = keys.unsqueeze(-1) # batch_size x seq_len x 64 x 8 x 1
+        keys = keys.transpose(1,2) # batch_size x 64 x seq_len x 8 x 1
+        queries = queries.permute(0, 3, 4, 1, 2)  # [batch_size, 8,2, seq_len, 64]
+        keys = keys.permute(0, 3, 4, 1, 2) # batch_size x 8 x 1 x 64 x seq_len
 
-        #compute attention scores (CHECK THIS)
-        attn_scores = queries*keys #element-wise multiplication with broadcasting
-        
+        #compute attention scores
+        attn_scores = queries @ keys #batch_size x 8 x 2 x seq_len x seq_len
+                        
         #mask?
 
-        #compute context vector
-        context_vector = attn_scores*values
+        #compute attention weights
+        attn_weights = torch.softmax(attn_scores/hidden_dim**0.5, dim=-1) # batch_size x 8 x 2 x seq_len x seq_len
 
+        #compute context vector
+        values =values.unsqueeze(-1) # batch_size x seq_len x 64 x 8 x 1
+        values = values.transpose(0,3,4,1,2) # batch_size x 8 x 1 x seq_len x 64
+        context_vector = attn_weights @ values # batch_size x 8 x 2 x seq_len x 64
+        context_vector = context_vector.view(batch_size, self.num_heads, seq_len, hidden_dim//self.num_heads) # batch_size x 16 x seq_len x 64
+        context_vector = context_vector.permute(0,2,1,3) # batch_size x seq_len x 16 x 64
+        
         #concatenate heads
+        context_vector = context_vector.view(batch_size, seq_len, hidden_dim) # batch_size x seq_len x 1024
 
         #apply linear output layer
-
+        context_vector = self.linear_output_layer(context_vector) # batch_size x seq_len x 1024
+        
+ 
         return context_vector
         
 
