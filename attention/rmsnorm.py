982036b0503f255713f9be2c17516a77c6bafa03
juannpmari <juannpmari@gmail.com>
2025-10-25 11:37:21 -0300

    fix RMSNorm computation

--- attention/rmsnorm.py
+++ attention/rmsnorm.py
@@ -1,5 +1,11 @@
 import torch
 
-def compute_rmsnorm(x: torch.tensor) -> torch.tensor:
-    rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True))
-    return x / rms
+class RMSNorm(nn.Module):
+    def __init__(self, hidden_dim: int):
+        super().__init__()
+        self.epsilon = 1e-5
+        self.gain = torch.nn.Parameter(torch.ones(hidden_dim))
+        
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.epsilon)
+        return self.gain * x/rms
