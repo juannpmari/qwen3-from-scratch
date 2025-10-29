c81f787aeb0b8a2ede279f39740de7e31b315100
juannpmari <juannpmari@gmail.com>
2025-10-29 10:38:30 -0300

    Implemented SGD optimizer

--- common/metrics.py
+++ common/metrics.py
@@ -1,22 +1,5 @@
 import torch
 
-def compute_cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-    """
-    logits: (batch_size, context_length, vocab_size) CHECK
-    targets: (batch_size, context_length) IDs of the target tokens
-
-    Returns:
-        ce: (batch_size, context_length) cross entropy for each token of each sequence in the batch
-    """
-    ce = 0
-    for b in range(logits.shape[0]): #D
-        for i in range(targets.shape[1]): #i
-            target = targets[b][i]
-            prob = torch.exp(logits[b][i][target])/sum(torch.exp(logits[b][i]))
-            ce += -torch.log(prob)
-    ce = ce / (logits.shape[0] * logits.shape[1])
-    return ce # (batch_size, context_length)
-
 def compute_perplexity(cross_entropy: torch.Tensor) -> torch.Tensor:
     """
     cross_entropy: (batch_size, context_length)
--- train/optimizer.py
+++ train/optimizer.py
@@ -1,19 +1,43 @@
 from torch.optim import Optimizer
+from typing import Optional, Callable
+import math
 
 class SGD(Optimizer):
+    """
+    Stochastic gradient descent optimizer, implements learning rate decay.
+    """
     def __init__(self, params, lr=1e-3):
-        super().__init__(params, lr=lr)
-        pass
+        if lr <= 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        default = {"lr": lr}
+        super().__init__(params, default)
 
-    def step(self):
-        pass
+
+    def step(self, closure:Optional[Callable] = None):
+        loss = None if closure is None else closure() #not used now but required by torch interface.
+        for group in self.param_groups:
+            lr = group['lr'] # Get the learning rate.
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p] # Get state associated with p.
+                t = state.get("t", 0) # Get iteration number from the state, or initial value.
+                grad = p.grad.data # Get the gradient of loss with respect to p.
+                p.data -= lr/math.sqrt(t+1) * grad # Update weight tensor in-place (SGD variant that implements learning rate decay).
+                state["t"] = t + 1 # Increment iteration number.
+        return loss
+
+
 
 class AdamW(Optimizer):
+    """
+    AdamW optimizer.
+    """
     def __init__(self, params, lr=1e-3):
         super().__init__(params, lr=lr)
         pass
 
-    def step(self):
+    def step(self, closure:Optional[Callable] = None):
         pass
 
 class LRScheduler:
