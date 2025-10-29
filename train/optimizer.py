a1dff17af25cf282edaa0391898bc18f571ad1fc
juannpmari <juannpmari@gmail.com>
2025-10-29 11:17:45 -0300

    implemented AdamW

--- train/optimizer.py
+++ train/optimizer.py
@@ -1,6 +1,8 @@
+import torch
 from torch.optim import Optimizer
 from typing import Optional, Callable
 import math
+
 
 class SGD(Optimizer):
     """
@@ -28,17 +30,44 @@ class SGD(Optimizer):
         return loss
 
 
-
 class AdamW(Optimizer):
     """
     AdamW optimizer.
     """
-    def __init__(self, params, lr=1e-3):
-        super().__init__(params, lr=lr)
-        pass
+    def __init__(self, params, lr=1e-3, b1=0.9, b2=0.95, eps=1e-08, w_decay = 0.01):
+        if lr <= 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        default = {"lr": lr, "b1": b1, "b2": b2, "eps": eps, "w_decay": w_decay}
+        super().__init__(params, default)
+        for p in params:
+            self.state[p]['m'] = torch.zeros_like(p)
+            self.state[p]['v'] = torch.zeros_like(p)
 
     def step(self, closure:Optional[Callable] = None):
-        pass
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group['lr']
+            b1 = group['b1']
+            b2 = group['b2']
+            eps = group['eps']
+            w_decay = group['w_decay']
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                grad = p.grad.data
+                m = b1*state['m'] + (1-b1)*grad
+                v = b2*state['v'] + (1-b2)*grad**2
+                state['m'] = m
+                state['v'] = v
+
+                t = state.get("t", 1)
+                lr = lr*math.sqrt(1-b2**t)/(1-b1**t)
+                p.data -= lr*m/math.sqrt(v+eps)
+                p.data -= lr*w_decay*p.data
+                state["t"] = t + 1
+        return loss
+
 
 class LRScheduler:
     pass
