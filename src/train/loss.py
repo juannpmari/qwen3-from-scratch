Fix cross entropy computation Replace torch.softmax + torch.log by torch.log_softmax for numerical stability

src/train/loss.py
@@ -2,15 +2,19 @@ import torch
 
 def compute_cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     """
-    logits: (batch_size, vocab_size) CHECK
+    logits: (batch_size, vocab_size)
     targets: (batch_size,) IDs of the target tokens
 
     Returns:
-        ce: (batch_size,) cross entropy for each sequence in the batch
+        ce: scalar; avg cross entropy for the sequences in the batch
     """
-    log_probs = torch.log_softmax(logits, dim=2)
-    ce = -log_probs.gather(2, targets.unsqueeze(2)).squeeze(2)
-    return ce  # (batch_size, context_length)
+    ce = torch.zeros(logits.shape[0])
+    for b in range(logits.shape[0]): #iterate over batch
+        sequence = logits[b] #vocab_size
+        sequence_log_prob = torch.log_softmax(sequence, dim=0)
+        target_log_prob = sequence_log_prob[targets[b]]
+        ce[b] = -target_log_prob
+    return ce.mean()  #scalar
 
 
     #uv run pytest -k test_cross_entropy