f6b7c201321352d0086e1d3b41997bdfecab6d86
juannpmari <juannpmari@gmail.com>
2025-11-06 11:56:14 -0300

    update generation loop

--- src/inference/generate.py
+++ src/inference/generate.py
@@ -10,16 +10,18 @@ def generate(model:nn.Module, tokenizer:BPETokenizer, prompt: str, max_tokens: i
     device = next(model.parameters()).device
     generated_tokens = 0
     end_of_sequence = False
+    token_ids = tokenizer.encode(prompt)
     
     while generated_tokens < max_tokens and not end_of_sequence:
-        token_ids = tokenizer.encode(prompt)
         token_ids = torch.tensor(token_ids).unsqueeze(0).to(device)
         logits = model(token_ids)
         probs = softmax(logits, dim=-1, temperature=temperature)
 
         token_id = _sample(probs, top_p=top_p)
         generated_tokens += 1
-        prompt += tokenizer.decode(token_id)
+        token_ids.append(token_id)
 
-    return prompt
+    response = ''.join([char for char in tokenizer.decode(token_id)])
+
+    return response
         
